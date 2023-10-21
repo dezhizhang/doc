acc5b1a6e64169c78742f0b9edb4651290a938a4
dezhizhang <1541609448@qq.com>
2023-10-21 23:06:00 +0800

    feat: docker文件

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,3 +1,12 @@
+/*
+ * :file description: 
+ * :name: /dumi/.dumirc.ts
+ * :author: 张德志
+ * :copyright: (c) 2023, Tungee
+ * :date created: 2023-10-21 21:31:50
+ * :last editor: 张德志
+ * :date last edited: 2023-10-21 22:09:27
+ */
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
--- docs/guide.md
+++ docs/guide.md
@@ -5,3 +5,15 @@
 ```
 docker exec --it 容器id /bin/bash
 ```
+
+### 安装 tomcat
+
+```
+docker pull tomcat
+```
+
+### 运行 tomcat
+
+```
+docker run -d -p 8080:8080 --name t1 tomcat
+```
